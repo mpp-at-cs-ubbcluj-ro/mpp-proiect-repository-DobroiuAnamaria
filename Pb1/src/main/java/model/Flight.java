75249d03bfdada8089229e5ccc1152595fd26138
aymphyro <dobroiu_anamaria@yahoo.com>
2022-03-09 12:17:38 +0200

    editare flight si C#

--- Pb1/src/main/java/model/Flight.java
+++ Pb1/src/main/java/model/Flight.java
@@ -5,58 +5,81 @@ import java.util.Objects;
 public class Flight extends Entity<Integer>{
 
     String destiantion;
+    String arrival;
     String dateDeparture;
     String timeDeparture;
     String airport;
     int numberOfplace;
 
-    public Flight(String destiantion, String dateDeparture, String timeDeparture, String airport, int numberOfplace) {
+    public Flight(String destiantion, String arrival, String dateDeparture, String timeDeparture, String airport, int numberOfplace) {
         this.destiantion = destiantion;
+        this.arrival = arrival;
         this.dateDeparture = dateDeparture;
         this.timeDeparture = timeDeparture;
         this.airport = airport;
         this.numberOfplace = numberOfplace;
-
     }
 
     public String getDestiantion() {
         return destiantion;
     }
 
-    public String getDateDeparture() {
-        return dateDeparture;
-    }
-
-    public String getTimeDeparture() {
-        return timeDeparture;
-    }
-
-    public String getAirport() {
-        return airport;
-    }
-
-    public int getNumberOfplace() {
-        return numberOfplace;
-    }
-
     public void setDestiantion(String destiantion) {
         this.destiantion = destiantion;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getDateDeparture() {
+        return dateDeparture;
     }
 
     public void setDateDeparture(String dateDeparture) {
         this.dateDeparture = dateDeparture;
     }
 
+    public String getTimeDeparture() {
+        return timeDeparture;
+    }
+
     public void setTimeDeparture(String timeDeparture) {
         this.timeDeparture = timeDeparture;
+    }
+
+    public String getAirport() {
+        return airport;
     }
 
     public void setAirport(String airport) {
         this.airport = airport;
     }
 
+    public int getNumberOfplace() {
+        return numberOfplace;
+    }
+
     public void setNumberOfplace(int numberOfplace) {
         this.numberOfplace = numberOfplace;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "destiantion='" + destiantion + '\'' +
+                ", arrival='" + arrival + '\'' +
+                ", dateDeparture='" + dateDeparture + '\'' +
+                ", timeDeparture='" + timeDeparture + '\'' +
+                ", airport='" + airport + '\'' +
+                ", numberOfplace=" + numberOfplace +
+                '}';
     }
 
     @Override
@@ -64,22 +87,14 @@ public class Flight extends Entity<Integer>{
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Flight flight = (Flight) o;
-        return numberOfplace == flight.numberOfplace && Objects.equals(destiantion, flight.destiantion) && Objects.equals(dateDeparture, flight.dateDeparture) && Objects.equals(timeDeparture, flight.timeDeparture) && Objects.equals(airport, flight.airport);
+        return numberOfplace == flight.numberOfplace && Objects.equals(destiantion, flight.destiantion) && Objects.equals(arrival, flight.arrival) && Objects.equals(dateDeparture, flight.dateDeparture) && Objects.equals(timeDeparture, flight.timeDeparture) && Objects.equals(airport, flight.airport);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(destiantion, dateDeparture, timeDeparture, airport, numberOfplace);
+        return Objects.hash(destiantion, arrival, dateDeparture, timeDeparture, airport, numberOfplace);
     }
 
-    @Override
-    public String toString() {
-        return "Flight{" +
-                "destiantion='" + destiantion + '\'' +
-                ", dateDeparture='" + dateDeparture + '\'' +
-                ", timeDeparture='" + timeDeparture + '\'' +
-                ", airport='" + airport + '\'' +
-                ", numberOfplace=" + numberOfplace +
-                '}';
-    }
+
+
 }
