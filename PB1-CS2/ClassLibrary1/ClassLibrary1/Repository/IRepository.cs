ba5e326debc2f525fb56775a1a538705e29ab473
aymphyro <dobroiu_anamaria@yahoo.com>
2022-03-10 22:08:18 +0200

    C#-tema

---
+++ PB1-CS2/ClassLibrary1/ClassLibrary1/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ClassLibrary1.Model;
+
+namespace ClassLibrary1.Repository
+{
+    public interface IRepository <in TId, TE> where TE : Entity<TId>
+    {
+
+        TE FindOne(TId id);
+        IEnumerable<TE> FindAll();
+        TE Save(TE entity);
+        TE Delete(TId id);
+        TE Update(TE entity);
+
+    }
+}
---
+++ PB1-CS2/ClassLibrary1/ClassLibrary1/Repository/RepositoryFlight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ClassLibrary1.Model;
+
+namespace ClassLibrary1.Repository
+{
+    public abstract class RepositoryFlight: IRepository<int, Flight>
+    {
+        public abstract Flight FindOne(int id);
+        public abstract IEnumerable<Flight> FindAll();
+        public abstract Flight Save(Flight entity);
+        public abstract Flight Delete(int id);
+        public abstract Flight Update(Flight entity);
+        
+    }
+}
